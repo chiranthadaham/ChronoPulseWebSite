6ee462bca9d76f2c27a93cde70b57ee02bfa50f5
chiranthadaham <145154710+chiranthadaham@users.noreply.github.com>
2025-07-25 11:08:24 +0530

    Update 3.0
    Added Dark theme

---
+++ darkmode.js
@@ -0,0 +1,55 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch=document.getElementById('theme-switch')
+const logo=document.getElementById('header-img')
+const footerLogo=document.getElementById('footer-img')
+const darkcard=document.getElementsByClassName('feature-card')
+const footer=document.getElementById('footer')
+const pricecards=document.getElementsByClassName('card')
+
+
+const enableDarkmode=()=>{
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode','active')
+    if(logo) 
+        logo.src='logo-orange.png'
+    if(footerLogo)
+        footerLogo.src='logo-light.png'
+    if(darkcard)
+        for (let i = 0; i < darkcard.length; i++) {
+            darkcard[i].style.backgroundColor = '#00091bff';
+        }
+    if(footer)
+        footer.style.backgroundColor='#00091bff';
+    if(pricecards)
+        for(let i=0;i<pricecards.length;i++){
+            pricecards[i].style.backgroundColor='#00091bff';
+        }
+    
+
+   
+}
+
+const disabledarkmode=()=>{
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode',null)
+    if(logo) 
+        logo.src='logo.png'
+    if(footerLogo)
+        footerLogo.src='logo-light.png'
+    for (let i = 0; i < darkcard.length; i++) {
+            darkcard[i].style.backgroundColor = '#ffffffff';
+        }
+    if(footer)
+        footer.style.backgroundColor='#2d2d3a';
+    if(pricecards)
+        for(let i=0;i<pricecards.length;i++){
+            pricecards[i].style.backgroundColor='#ffffffff';
+        }
+}
+
+if(darkmode==="active")enableDarkmode()
+
+themeSwitch.addEventListener("click",()=>{
+    darkmode=localStorage.getItem('darkmode')
+    darkmode!=="active"? enableDarkmode():disabledarkmode()
+})
